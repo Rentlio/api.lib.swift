2d73767b2dcc7e87fcfe1db6d1876d9d378c8a1b
Juraj Hilje <jhilje@gmail.com>
2017-06-16 11:53:54 +0200

    Add example

--- api.lib.swift/ViewController.swift
+++ api.lib.swift/ViewController.swift
@@ -1,9 +1,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let rentlio = RentlioApi(apikey: "your_apikey")
+        
+        rentlio.getUser(
+            done: {
+                res in
+                if let json = res.json {
+                    print(json)
+                }
+            }
+        )
     }
     
 }
